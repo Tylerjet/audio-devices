f5f3cdc48d4bf36e6c79b42e1c0b2a0f9fade517
MrTylerjet <MrTylerjet@gmail.com>
2020-04-17 21:58:35 -0700

    fixed module exports

--- actions/setVolume.js
+++ actions/setVolume.js
@@ -1,6 +1,6 @@
 const soundvolumeview = require('soundvolumeview-node')
 
-module.exports = module.exports = (manufacturer, audioDevice, direction, volume) => {
+module.exports = (manufacturer, audioDevice, direction, volume) => {
 // manufacturer (Name in parenthesis of getdevices), audioDevice (device name before parenthesis of get devices)
 // direction capture or render (capture = recording device, render = playback device)
 // Reference SoundVolumeView.exe for explaination of direction
