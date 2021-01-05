1f3ab3fb3aa3f3f527d4f18676e6fb1312e9e57c
MrTylerjet <MrTylerjet@gmail.com>
2021-01-04 19:50:51 -0700

    removed application variable from setfocuseddevice

--- actions/setFocusedDevice.js
+++ actions/setFocusedDevice.js
@@ -1,6 +1,6 @@
 const soundvolumeview = require('soundvolumeview-node')
 
-module.exports = (manufacturer, audioDevice, direction, application) => {
+module.exports = (manufacturer, audioDevice, direction) => {
 // manufacturer (Name in parenthesis of getdevices), audioDevice (device name before parenthesis of get devices)
 // direction capture or render (capture = recording device, render = playback device)
 // Reference SoundVolumeView.exe for explaination of direction
